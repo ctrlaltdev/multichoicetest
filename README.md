8a8b7f61e6dc6caba2764e1b3bc8d2a9ac3e188c
Yorick <yorick@ctrlaltdev.xyz>
2018-06-15 19:16:24 -0700

    wordking version

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# multichoicetest
-A small javascript library for generating multiple choice test
+<a name="multiChoice"></a>
+
+## multiChoice
+**Kind**: global class
+
+* [multiChoice](#multiChoice)
+    * [new multiChoice()](#new_multiChoice_new)
+    * _instance_
+        * [.createForm([questions], [domElem])](#multiChoice+createForm)
+        * [.shuffleAnswers(array)](#multiChoice+shuffleAnswers) ⇒ <code>Array</code>
+        * [.checkAnswer(e)](#multiChoice+checkAnswer)
+        * [.calcScore()](#multiChoice+calcScore)
+        * [.printMsg(score, [msgs])](#multiChoice+printMsg)
+        * [.setEventListeners([domElem])](#multiChoice+setEventListeners)
+    * _static_
+        * [.multiChoice](#multiChoice.multiChoice)
+            * [new multiChoice(questions, scoreMsgs, domElem)](#new_multiChoice.multiChoice_new)
+
+<a name="new_multiChoice_new"></a>
+
+### new multiChoice()
+Create a multiple choice test
+
+<a name="multiChoice+createForm"></a>
+
+### multiChoice.createForm([questions], [domElem])
+Add a form to the DOM based on the given questions
+
+**Kind**: instance method of [<code>multiChoice</code>](#multiChoice)
+
+| Param | Type | Default |
+| --- | --- | --- |
+| [questions] | <code>\*</code> | <code>this.questions</code> |
+| [domElem] | <code>\*</code> | <code>this.domElem</code> |
+
+<a name="multiChoice+shuffleAnswers"></a>
+
+### multiChoice.shuffleAnswers(array) ⇒ <code>Array</code>
+The Fisher-Yates (aka Knuth) shuffle
+
+**Kind**: instance method of [<code>multiChoice</code>](#multiChoice)
+**Returns**: <code>Array</code> - shuffled array
+
+| Param | Type | Description |
+| --- | --- | --- |
+| array | <code>Array</code> | array that you want shuffled |
+
+<a name="multiChoice+checkAnswer"></a>
+
+### multiChoice.checkAnswer(e)
+Check if selected option is the right answer, then check if everything is answered
+
+**Kind**: instance method of [<code>multiChoice</code>](#multiChoice)
+
+| Param | Type |
+| --- | --- |
+| e | <code>\*</code> |
+
+<a name="multiChoice+calcScore"></a>
+
+### multiChoice.calcScore()
+Retrieve the number of correct answers
+
+**Kind**: instance method of [<code>multiChoice</code>](#multiChoice)
+<a name="multiChoice+printMsg"></a>
+
+### multiChoice.printMsg(score, [msgs])
+Print the end message depending on the user score
+
+**Kind**: instance method of [<code>multiChoice</code>](#multiChoice)
+
+| Param | Type | Default |
+| --- | --- | --- |
+| score | <code>\*</code> |  |
+| [msgs] | <code>\*</code> | <code>this.messages</code> |
+
+<a name="multiChoice+setEventListeners"></a>
+
+### multiChoice.setEventListeners([domElem])
+Set the event listeners to the inputs
+
+**Kind**: instance method of [<code>multiChoice</code>](#multiChoice)
+
+| Param | Type | Default |
+| --- | --- | --- |
+| [domElem] | <code>\*</code> | <code>this.domElem</code> |
+
+<a name="multiChoice.multiChoice"></a>
+
+### multiChoice.multiChoice
+**Kind**: static class of [<code>multiChoice</code>](#multiChoice)
+<a name="new_multiChoice.multiChoice_new"></a>
+
+#### new multiChoice(questions, scoreMsgs, domElem)
+Creates an instance of multiChoice.
+
+
+| Param | Type |
+| --- | --- |
+| questions | <code>Array.&lt;Object&gt;</code> |
+| scoreMsgs | <code>Array.&lt;Object&gt;</code> |
+| domElem | <code>HTMLElement</code> |
---
+++ src/multichoicetest.js
@@ -0,0 +1,216 @@
+import Haikunator from '../node_modules/haikunator/lib/haikunator.js'
+var haikunator = new Haikunator()
+
+/**
+ *Create a multiple choice test
+ * @class multiChoice
+ */
+class multiChoice {
+
+  /**
+   *Creates an instance of multiChoice.
+   * @param {!Array.<Object>} questions
+   * @param {!Array.<Object>} scoreMsgs
+   * @param {!HTMLElement} domElem
+   * @memberof multiChoice
+   */
+  constructor(questions, scoreMsgs, domElem) {
+    this.questions = questions
+    this.messages = scoreMsgs
+    this.domElem = domElem
+
+    this.validAnswers = {}
+    this.totalQuestions = 0
+
+    this.createForm()
+    this.setEventListeners()
+  }
+
+  /**
+   *Add a form to the DOM based on the given questions
+   * @param {*} [questions=this.questions]
+   * @param {*} [domElem=this.domElem]
+   * @memberof multiChoice
+   */
+  createForm(questions = this.questions, domElem = this.domElem) {
+
+    let form = document.createElement('form')
+    form.classList.add('multiChoiceTest')
+
+    questions.map((question) => {
+
+      let hashkuQuestion = haikunator.haikunate()
+
+      let fieldset = document.createElement('fieldset')
+      fieldset.id = hashkuQuestion
+
+      let questionElem = document.createElement('label')
+      let questionElemTxt = document.createTextNode(question.question)
+      questionElem.appendChild(questionElemTxt)
+
+      fieldset.appendChild(questionElem)
+
+      this.shuffleAnswers(question.answers).map((answer) => {
+
+        let hashkuAnswer = haikunator.haikunate()
+
+        if (answer.valid) {
+          this.validAnswers[hashkuQuestion] = hashkuAnswer
+        }
+
+        let div = document.createElement('div')
+        div.classList.add('multiChoiceAnswer')
+
+        let input = document.createElement('input')
+        input.setAttribute('type', 'radio')
+        input.id = hashkuAnswer
+        input.setAttribute('name', hashkuQuestion)
+        input.setAttribute('value', hashkuAnswer)
+        div.appendChild(input)
+
+        let inputContent = document.createElement('label')
+        inputContent.setAttribute('for', hashkuAnswer)
+        let inputContentTxt = document.createTextNode(answer.answer)
+        inputContent.appendChild(inputContentTxt)
+        div.appendChild(inputContent)
+
+        fieldset.appendChild(div)
+
+      })
+      
+      form.appendChild(fieldset)
+
+    })
+
+    domElem.appendChild(form)
+
+    this.totalQuestions = questions.length
+
+  }
+
+  /**
+   *The Fisher-Yates (aka Knuth) shuffle
+   * @param {!Array} array array that you want shuffled
+   * @returns {Array} shuffled array
+   * @memberof multiChoice
+   */
+  shuffleAnswers(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex] = temporaryValue
+    }
+
+    return array
+  }
+
+  /**
+   *Check if selected option is the right answer, then check if everything is answered
+   * @param {*} e
+   * @memberof multiChoice
+   */
+  checkAnswer(e) {
+    let target = e.target
+    let name = target.getAttribute('name')
+    let value = target.getAttribute('value')
+
+    if (this.validAnswers[name] == value) {
+
+      let inputs = this.domElem.querySelectorAll('.multiChoiceAnswer input[name="'+name+'"]');
+      [].forEach.call(inputs, (input) => {
+        input.disabled = true
+      })
+
+      target.classList.add('correct')
+      target.classList.add('selected')
+
+      this.domElem.querySelector('#'+name).classList.add('answered')
+
+    } else {
+
+      let inputs = this.domElem.querySelectorAll('.multiChoiceAnswer input[name="'+name+'"]');
+      [].forEach.call(inputs, (input) => {
+        input.disabled = true
+      })
+
+      target.classList.add('wrong')
+      target.classList.add('selected')
+
+      this.domElem.querySelector('.multiChoiceAnswer input[value="'+this.validAnswers[name]+'"]').classList.add('correct')
+
+      this.domElem.querySelector('#'+name).classList.add('answered')
+
+    }
+
+    let answered = this.domElem.querySelectorAll('fieldset.answered')
+
+    if (this.totalQuestions == answered.length) {
+      this.calcScore()
+    }
+
+  }
+
+  /**
+   *Retrieve the number of correct answers
+   * @memberof multiChoice
+   */
+  calcScore() {
+    let correctAnswers = this.domElem.querySelectorAll('input.selected.correct').length
+
+    this.printMsg(correctAnswers)
+  }
+
+  /**
+   *Print the end message depending on the user score
+   * @param {*} score
+   * @param {*} [msgs=this.messages]
+   * @memberof multiChoice
+   */
+  printMsg(score, msgs = this.messages) {
+    msgs.map((msg) => {
+
+      if (score >= msg.scoreThresold) {
+        let scoreMsg = document.createElement('div')
+        scoreMsg.classList.add('scoreMsg')
+
+        let msgTitle = document.createElement('p')
+        msgTitle.classList.add('msgTitle')
+        let msgTitleTxt = document.createTextNode(msg.title)
+        msgTitle.appendChild(msgTitleTxt)
+        scoreMsg.appendChild(msgTitle)
+
+        let msgContent = document.createElement('p')
+        let msgContentTxt = document.createTextNode(msg.message)
+        msgContent.appendChild(msgContentTxt)
+        scoreMsg.appendChild(msgContent)
+
+        this.domElem.appendChild(scoreMsg)
+      }
+
+    })
+  }
+
+  /**
+   *Set the event listeners to the inputs
+   * @param {*} [domElem=this.domElem]
+   * @memberof multiChoice
+   */
+  setEventListeners(domElem = this.domElem) {
+    let answers = domElem.querySelectorAll('.multiChoiceAnswer input');
+    [].forEach.call(answers, (answer) => {
+      answer.addEventListener('click', this.checkAnswer.bind(this))
+    })
+  }
+
+}
+
+export default multiChoice
